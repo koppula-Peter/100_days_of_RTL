070299b96b0aa96f9332dcbcebca282daa9c1ee5
koppula-Peter <149902115+koppula-Peter@users.noreply.github.com>
2024-11-07 23:13:07 +0530

    Update RTL_design_B2G.v

--- Day 4: Binary_to_Gray/RTL_design_B2G.v
+++ Day 4: Binary_to_Gray/RTL_design_B2G.v
@@ -1,14 +1,12 @@
-//--------------------------------gray to binary-------------------------------------
+//--------------------------------binary to gray-------------------------------------
 
-module gray_to_binary #(parameter N=16)(in,out);
+
+module binary_to_gray #(parameter N=16)(in,out);
 input [N-1:0]in;
 output [N-1:0]out;
 
-reg [N-1:0] in_reg=0;
-
-always @(in)
-    in_reg=in;
-
+//reg [N-1:0] in_reg;
+//assign out=1;
 //xor_gate g0(.in_1(in[0]),.in_2(0),.out(out[0]));
 
 genvar i;
@@ -20,16 +18,16 @@ genvar i;
                     assign out[N-1-i]=in[N-1-i];
                 else
                     //xor_gate g0(.in_1(in[i-1]),.in_2(in[i]),.out(out[i]));
-                    assign out[N-1-i]=in[N-1-i] ^ out[N-i];
+                    assign out[N-1-i]=in[N-1-i] ^ in[N-i];
             end
     endgenerate
 endmodule
+
 //-----------------------------------------------------------------------------------------
 
 module xor_gate(in_1,in_2,out);
-input in_1,in_2;
+input in_1,in_2; 
 output out;
-
 assign out=in_1 ^ in_2;
 
 endmodule
