77d09c7bd9fa66ce2f3338dc84d19f0464c6bbe2
koppula-Peter <149902115+koppula-Peter@users.noreply.github.com>
2024-12-18 22:05:08 +0530

    Update TB.v

--- Day 8 : Latches/TB.v
+++ Day 8 : Latches/TB.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 1ps / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Testbench for Latches Module
 //////////////////////////////////////////////////////////////////////////////////
@@ -8,6 +8,7 @@ module latches_tb;
     // Inputs
     reg a;
     reg b;
+    reg preset,clr;
 
     // Outputs
     wire sr_q_nor, sr_qn_nor, sr_q_nand, sr_qn_nand;
@@ -29,64 +30,73 @@ module latches_tb;
         .d_q_nor(d_q_nor), 
         .d_qn_nor(d_qn_nor), 
         .d_q_nand(d_q_nand), 
-        .d_qn_nand(d_qn_nand)
+        .d_qn_nand(d_qn_nand),
+        .preset(preset),
+        .clr(clr)
     );
 
     initial begin
         // Initialize Inputs
         $display("Starting Simulation");
-
-        a = 0; b = 0;
+         preset=0; clr=1;
         #10;
-        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
-        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
+         preset=0; clr=0;
+         
+        #1;  a = 0; b = 0;
+        #1;  a = 0; b = 1;
+        #1;  a = 1; b = 0;
+        #1;  a = 1; b = 1;
+        #1;
+        //$display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
+        $monitor("[jk NOR] j=%b k=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
+//        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
 
-        a = 0; b = 1;
-        #10;
-        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
-        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
+//        a = 0; b = 1;
+//        #10;
+//        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
+//        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
 
-        a = 1; b = 0;
-        #10;
-        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
-        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
+//        a = 1; b = 0;
+//        #10;
+//        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
+//        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
 
-        a = 1; b = 1;
-        #10;
-        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
-        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
+//        a = 1; b = 1;
+//        #10;
+//        $display("[SR NOR] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nor, sr_qn_nor);
+//        $display("[SR NAND] a=%b b=%b => Q=%b Qn=%b", a, b, sr_q_nand, sr_qn_nand);
 
-        // Test JK Latch
-        a = 0; b = 0;
-        #10;
-        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
-        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
+//        // Test JK Latch
+//        a = 0; b = 0;
+//        #10;
+//        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
+//        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
 
-        a = 0; b = 1;
-        #10;
-        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
-        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
+//        a = 0; b = 1;
+//        #10;
+//        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
+//        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
 
-        a = 1; b = 0;
-        #10;
-        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
-        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
+//        a = 1; b = 0;
+//        #10;
+//        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
+//        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
 
-        a = 1; b = 1;
-        #10;
-        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
-        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
+//        a = 1; b = 1;
+//        #10;
+//        $display("[JK NOR] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nor, jk_qn_nor);
+//        $display("[JK NAND] a=%b b=%b => Q=%b Qn=%b", a, b, jk_q_nand, jk_qn_nand);
 
-        // Test D Latch
-        a = 0; b = 0;
-        #10;
-        $display("[D NOR] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nor, d_qn_nor);
-        $display("[D NAND] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nand, d_qn_nand);
+//        // Test D Latch
+//        a = 0; b = 0;
+//        #10;
+//        $display("[D NOR] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nor, d_qn_nor);
+//        $display("[D NAND] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nand, d_qn_nand);
 
-        a = 1; b = 0;
-        #10;
-        $display("[D NOR] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nor, d_qn_nor);
-        $display("[D NAND] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nand, d_qn_nand);
+//        a = 1; b = 0;
+//        #10;
+//        $display("[D NOR] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nor, d_qn_nor);
+//        $display("[D NAND] a=%b b=%b => Q=%b Qn=%b", a, b, d_q_nand, d_qn_nand);
 
         // End simulation
         $finish;
